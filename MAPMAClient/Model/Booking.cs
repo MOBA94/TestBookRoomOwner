2a626de697e4ba5c47d0221551c3a831ed520e00
Brygger92 <48904202+Brygger92@users.noreply.github.com>
2020-04-16 20:18:26 +0200

    Oprettelse af 3 classer i Servicelaget

--- MAPMAClient/Model/Booking.cs
+++ MAPMAClient/Model/Booking.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//klar til arbejde emd Ranz
 namespace MAPMAClient.Model {
     public class Booking {
 
