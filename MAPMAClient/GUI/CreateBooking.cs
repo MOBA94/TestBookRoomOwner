89d9bdc31b3f640481008c8da3701cc50ae01e79
Mads Gram Ranzau <1078051@ucn.dk>
2020-04-17 08:48:04 +0200

    GUI For Create Booking

---
+++ MAPMAClient/GUI/CreateBooking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MAPMAClient.GUI {
+    public partial class CreateBooking : Form {
+        public CreateBooking() {
+            InitializeComponent();
+        }
+    }
+}
